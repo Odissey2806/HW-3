ffc0844d5d7a23a40a60b45a1c4e6cb54f163c46
Odissey2806 <aleksand2806@ya.ru>
2024-10-20 18:48:36 +0700

    ready-made homework

--- src/Main.java
+++ src/Main.java
@@ -16,13 +16,13 @@ public class Main {
         System.out.println("«Значение переменной g с типом int равно " + g + "»" );
 
         // Задача №2
-        float x = 27.12f;
-        long y = 987_678_965_549L;
-        double z = 2.786;
-        short h = 569;
-        short r = -159;
-        int k = 27_897;
-        byte s = 67;
+        float flo = 27.12f;
+        long lon = 987_678_965_549L;
+        double doub = 2.786;
+        short shor = 569;
+        short stop = -159;
+        int in = 27_897;
+        byte byt = 67;
 
         // Задача №3
         short lyudmilaPavlovnaStudent = 23;
